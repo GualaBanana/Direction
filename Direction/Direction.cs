33c47e42c0cdb10ba4c8af46f3a79870de39693a
GualaBanana <tem4rik@gmail.com>
2022-01-11 02:07:33 -0800

    Initial commit.

---
+++ Direction/Direction.cs
@@ -0,0 +1,10 @@
+﻿namespace Structures
+{
+    public enum Direction
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+}
---
+++ Direction/DirectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Structures
+{
+    public static class DirectionExtensions
+    {
+        public static Direction OppositeDirection(this Direction direction) => direction switch
+        {
+            Direction.Up => Direction.Down,
+            Direction.Left => Direction.Right,
+            Direction.Down => Direction.Up,
+            Direction.Right => Direction.Left,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
